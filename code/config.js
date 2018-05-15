21c432d11f56777f4e3efaec9245afc90d26a417
glewsimo <glewsimo@ecs.vuw.ac.nz>
2018-05-15 23:47:10 +1200

    update to new mongo

--- code/config.js
+++ code/config.js
@@ -1,13 +1,13 @@
 var config = {
     port: 52723,
     mongo: {
-        hosts: 'awall-shard-00-00-ahazm.mongodb.net:27017,awall-shard-00-01-ahazm.mongodb.net:27017,awall-shard-00-02-ahazm.mongodb.net',
+        hosts: 'engr489awall-shard-00-00-ahazm.mongodb.net:27017,engr489awall-shard-00-01-ahazm.mongodb.net:27017,engr489awall-shard-00-02-ahazm.mongodb.net',
         port: 27017,
         user: 'dev',
         password: 'Hblvb7YxspI5DsEn',
         db: 'aWall',
         ssl: true,
-        replicaSet: 'aWall-shard-0',
+        replicaSet: 'ENGR489aWall-shard-0',
         authSource: 'admin'
     }
 }
